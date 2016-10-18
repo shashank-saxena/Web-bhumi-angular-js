5549a2fefa4d6a3332985f9d7a3bcd51b184d8fe
jugalkishore23 <jugalkishore69@gmail.com>
2016-10-18 21:29:32 +0530

    task

---
+++ Jugal/task1/index.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+<script src="angularjs.js"></script>
+<script src="script.js"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+</head>	
+<body ng-app="myModule">
+<h1></h1>
+	<div ng-controller="myController">
+	
+	</div>
+
+	<div ng-controller="bookctrl">
+		<div class="col-lg-4" ng-repeat="b in book.Books">
+		<div ng-show="true">
+			<h3 class="text-center">{{b.ID}}</h3>
+			<img src="{{b.Image}}" width="100%" >
+			<div class="desc">{{b.Description}}</div>
+		</div>
+		
+		</div>
+	</div>
+</body>
+</html>
