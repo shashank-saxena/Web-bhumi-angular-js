3ee1809674a4b295e71208947fc31330c2b4f1e2
jugalkishore23 <jugalkishore69@gmail.com>
2016-10-20 23:29:02 +0530

    cart by javascript

---
+++ Jugal/task/index.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<script src="angularjs.js"></script>
+<script src="script.js"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script>
+var ob = new checkIt();
+ob.pubFunction();
+</script>
+</head>	
+<body>
+<div class="container">
+<div class="row">
+</div>
+</div>
+</body>
+</html>
